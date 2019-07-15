c3270806d9ba04f7caff8f85397aab0de921dab7
Nasim Kabiliravi <kabiliravi@gmail.com>
2019-07-14 22:23:13 -0400

    Created a sample for local variable

--- TempuratureConverter.java
+++
@@ -1,8 +0,0 @@
-public class TempuratureConverter {
-  public static void main(String[] args) {
-      int tempInFahrenheit = 350;
-      float tempInCelsius = (tempInFahrenheit - 32f) * (5f/9f);
-      System.out.println("Fahrenheit: " + tempInFahrenheit + " ˚F");
-      System.out.println("Celsius: " + tempInCelsius + " ˚C");
-  }
-}
